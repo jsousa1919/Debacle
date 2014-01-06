dfed0a522f1a066edc52e970a2e4e3c724f3512b
Justin <jsousa1919@gmail.com>
2014-01-05 23:30:23 -0500

    Move opinions to directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,53 @@
 (function() {
 
-  $.app.controller("Main", function($scope, DataService) {
+  $.app.controller('Main', function($scope, DataService) {
     return $scope.user = DataService.user;
   });
 
-  $.app.controller("Headers", function($scope, DataService) {});
+  $.app.controller('Headers', function($scope, DataService) {});
 
-  $.app.controller("Debate", function($scope, DataService) {
+  $.app.controller('Debate', function($scope, DataService) {
     return $scope.debate = DataService.debate;
+  });
+
+  $.app.factory('Backend', function($http) {
+    return {
+      vote: function(val) {
+        return $http.post('/vote', {
+          val: val
+        });
+      }
+    };
+  });
+
+  $.app.directive('opinion', function(Backend) {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '\
+<div class="row box padded2 {{classes}}">\
+  <div class="padded1">\
+    <b class="black">{{ opinion.author }}</b>\
+    -\
+    <i class="faded">posted {{ opinion.date }}</i>\
+  </div>\
+  <div ng-bind-html="opinion.text"></div>\
+</div>\
+    ',
+      scope: {
+        opinion: '=object',
+        classes: '@'
+      },
+      link: function($scope, element, attrs) {
+        return $scope.vote = function(val) {
+          return Backend.vote(val).then(function(res) {
+            return opinion.vote = res;
+          }, function(err) {
+            return console.log('Vote failed: ' + err);
+          });
+        };
+      }
+    };
   });
 
 }).call(this);
--- templates/coffee/app.coffee
+++ templates/coffee/app.coffee
@@ -1,7 +1,40 @@
-$.app.controller "Main", ($scope, DataService) ->
+$.app.controller 'Main', ($scope, DataService) ->
   $scope.user = DataService.user
 
-$.app.controller "Headers", ($scope, DataService) ->
+$.app.controller 'Headers', ($scope, DataService) ->
 
-$.app.controller "Debate", ($scope, DataService) ->
+$.app.controller 'Debate', ($scope, DataService) ->
   $scope.debate = DataService.debate
+
+$.app.factory 'Backend', ($http) ->
+  return {
+    vote: (val) ->
+      return $http.post '/vote', {val: val}
+  }
+
+$.app.directive 'opinion', (Backend) ->
+  return {
+    restrict: 'E'
+    replace: true
+    template: '
+<div class="row box padded2 {{classes}}">
+  <div class="padded1">
+    <b class="black">{{ opinion.author }}</b>
+    -
+    <i class="faded">posted {{ opinion.date }}</i>
+  </div>
+  <div ng-bind-html="opinion.text"></div>
+</div>
+    ' # move to external file when on webserver
+    scope:
+      opinion: '=object'
+      classes: '@'
+    link: ($scope, element, attrs) ->
+      $scope.vote = (val) ->
+        Backend.vote(val)
+          .then(
+            (res) -> opinion.vote = res,
+            (err) -> console.log 'Vote failed: ' + err
+          )
+          
+  }
